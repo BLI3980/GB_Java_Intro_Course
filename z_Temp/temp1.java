42d6a6b56592b5b382fdec7fd8408cf241d9a476
Boris Li <liba8936@protonmail.com>
2022-12-08 20:09:28 +0500

    Lecture06 added

--- z_Temp/temp1.java
+++ z_Temp/temp1.java
@@ -1,8 +1,6 @@
 import javax.sound.sampled.Line;
 import java.io.StringReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.*;
 
 public class temp1 {
     public static void main(String[] args) {
@@ -36,20 +34,20 @@ public class temp1 {
 
 
 // ==================================================================
-        ArrayList<Integer> list_age = new ArrayList<>();
-        LinkedList<Integer> list_indx = new LinkedList<>();
-        list_age.add(45);
-        list_age.add(23);
-        list_age.add(99);
-        list_age.add(16);
-        list_age.add(38);
-        System.out.println(list_age);
-        list_indx.add(0);
-        list_indx.add(1);
-        list_indx.add(2);
-        list_indx.add(3);
-        list_indx.add(4);
-        System.out.println(list_indx);
+//        ArrayList<Integer> list_age = new ArrayList<>();
+//        LinkedList<Integer> list_indx = new LinkedList<>();
+//        list_age.add(45);
+//        list_age.add(23);
+//        list_age.add(99);
+//        list_age.add(16);
+//        list_age.add(38);
+//        System.out.println(list_age);
+//        list_indx.add(0);
+//        list_indx.add(1);
+//        list_indx.add(2);
+//        list_indx.add(3);
+//        list_indx.add(4);
+//        System.out.println(list_indx);
 
 // ==================================================================
 //        int cnt = list_age.size() - 1;
@@ -82,21 +80,56 @@ public class temp1 {
 //        }
 //        System.out.println(list_indx);
 // ==================================================================
-        int list_size = list_indx.size()-1;
-        for (int i = 0; i < list_size; i++) {
-            int min_value = (int) list_age.get(list_indx.get(i));
-            int min_ind = i;
-            for (int j = i+1; j <= list_size; j++) {
-                if((int) list_age.get(list_indx.get(j)) < min_value){
-                    min_value = (int) list_age.get(list_indx.get(j));
-                    min_ind = j;
-                }
-            }
-            int temp = list_indx.get(i);
-            list_indx.set(i,list_indx.get(min_ind));
-            list_indx.set(min_ind, temp);
-        }
-        System.out.println(list_indx);
+//        int list_size = list_indx.size()-1;
+//        for (int i = 0; i < list_size; i++) {
+//            int min_value = (int) list_age.get(list_indx.get(i));
+//            int min_ind = i;
+//            for (int j = i+1; j <= list_size; j++) {
+//                if((int) list_age.get(list_indx.get(j)) < min_value){
+//                    min_value = (int) list_age.get(list_indx.get(j));
+//                    min_ind = j;
+//                }
+//            }
+//            int temp = list_indx.get(i);
+//            list_indx.set(i,list_indx.get(min_ind));
+//            list_indx.set(min_ind, temp);
+//        }
+//        System.out.println(list_indx);
+// ==================================================================
+        Integer[] date = {4, 3, 2, 1, 0};
+        List<Integer> index_list = Arrays.asList(date);
+        List<Integer> index_list1 = Arrays.asList(date);
 
+        System.out.println(index_list);
+        Map<Integer, String> colors = new HashMap<>();
+        colors.put(3, "teal");
+        colors.put(1, "olive");
+        colors.put(0, "magenta");
+        colors.put(4, "beige");
+        colors.put(2, "plum");
+        Set<Integer> keys = colors.keySet();
+        int[] key_arr = new int[colors.size()-1];
+//        System.out.println(keys);
+        keys.toArray();
+        Arrays.sort(keys,keys.size(),0);
+
+        for (int i: colors.keySet()) {
+            key_arr[]
+        }
+//        colors.forEach((k,v) -> index_list1.add(k));
+//        for (int i: colors.keySet()) {
+//            index_list1.add(i);
+//        }
+//        System.out.println(index_list1);
+
+        System.out.println(colors.entrySet());
+
+
+        for (int i: index_list) {
+            System.out.println(colors.get(i));
+        }
+    }
+    static void Print(String str){
+        System.out.println(str);
     }
 }
